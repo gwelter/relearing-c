c2c88c69c0265a92336554badbcfbf83c70353f0
Guilherme Welter <guilherme.a.welter@gmail.com>
2020-10-09 19:15:02 -0300

    Guess the number

---
+++ guess_the_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+int guess_number() {
+    int number;
+    printf("Please guess a number between 0 and 20: ");
+    scanf("%d", &number);
+    return number;
+}
+
+int main() {
+    srand(time(NULL));
+
+    int tries = 5;
+    int number = rand() % 20;
+    bool guessed = false;
+    int guessed_number;
+
+    do
+    {
+        printf("You have %d tries\n", tries);
+        guessed_number = guess_number();
+        guessed = guessed_number == number;
+        if (guessed) {
+            printf("Congratulations, you guessed it!\n");
+        } else {
+            printf("Sorry, %d is %s than that, guess again\n", guessed_number, number > guessed_number ? "lower" : "greater");
+            --tries;
+        }
+    } while (tries > 0 && !guessed);
+}
